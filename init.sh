04550bba5b3eb2e347584944eb6df7ead876e1a0
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-08 21:25:56 +0900

    Add base shell

---
+++ init.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+set -ue
+
+curl -L -o provision_template.zip https://github.com/wataru0225/provision_template/archive/master.zip
+unzip provision_template.zip
+# tar xvfz provision_template.tar.gz
+cd provision_template-master
+sh install_mitamae.sh
+
+# if [ -n "$1" ]; then
+#   case $1 in
+#     "rails" ) bin/mitamae local roles/rails/default.rb ;;
+#     * ) echo "Please select role"
+#   esac
+# fi
