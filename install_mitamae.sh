8f869d3eda4b82eb03fc346fcfc3a0dc604825b5
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-08 21:04:46 +0900

    Add install_mitamae script

---
+++ install_mitamae.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+set -ue
+version="1.5.1"
+
+case "$(uname)" in
+  "Darwin")
+    file_name="mitamae-x86_64-darwin"
+    ;;
+  "Linux")
+    file_name="mitamae-x86_64-linux"
+    ;;
+  *)
+    echo "unknown uname: $(uname)"
+    exit 1
+    ;;
+esac
+
+wget "https://github.com/itamae-kitchen/mitamae/releases/download/v${version}/${file_name}"
+chmod +x ${file_name}
+mv ${file_name} mitamae
