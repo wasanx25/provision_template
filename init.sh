f1f4e6633d9b75fea0aec40409c0698cbeb8aef4
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-08 23:13:45 +0900

    Add tar command for linux

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
 #! /bin/sh
 set -ue
 
-curl -L -o provision_template.zip https://github.com/wataru0225/provision_template/archive/master.zip
-unzip provision_template.zip
-# tar xvfz provision_template.tar.gz
+
+case "$(uname)" in
+  "Darwin")
+    curl -L -o provision_template.zip https://github.com/wataru0225/provision_template/archive/master.zip
+    unzip provision_template.zip
+    ;;
+  "Linux")
+    curl -L -o provision_template.tar.gz https://github.com/wataru0225/provision_template/archive/master.tar.gz
+    tar xvfz provision_template.tar.gz
+    ;;
+  *)
+    echo "unknown uname: $(uname)"
+    exit 1
+    ;;
+esac
+
 cd provision_template-master
 sh install_mitamae.sh
 
