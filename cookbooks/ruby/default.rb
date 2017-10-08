e30c477115814a4f8f661b8dcf842ca822df1e38
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-08 19:17:30 +0900

    Add install ruby

---
+++ cookbooks/ruby/default.rb
@@ -0,0 +1,38 @@
+%w(
+  build-essential
+  curl
+  libcurl4-openssl-dev
+  libssl-dev
+  libssl-dev
+  libyaml-dev
+  stow
+  sudo
+  wget
+  zlib1g-dev
+).each do |name|
+  package name do
+    action :install
+  end
+end
+
+
+MINOR_VERSION = '2.4'
+MAINTENANCE_VERSION = '2.4.2'
+FILE_NAME = "ruby-#{MAINTENANCE_VERSION}"
+
+execute "wget http://ftp.ruby-lang.org/pub/ruby/#{MINOR_VERSION}/#{FILE_NAME}.tar.gz"
+execute "tar -zxvf #{FILE_NAME}.tar.gz"
+
+["./configure --prefix=/usr/local/stow/#{FILE_NAME}", 'make', 'make install'].each do |cmd|
+  execute cmd do
+    cwd FILE_NAME
+  end
+end
+
+execute "stow -Rv #{FILE_NAME}" do
+  cwd '/usr/local/stow'
+end
+
+execute "rm -rf #{FILE_NAME}"
+
+execute 'gem install bundler'
