68e9be1799a998225f5f18bc8ec0a1964762460d
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-09 12:57:00 +0900

    Add pkg update before apply recipe

--- roles/php-fpm/default.rb
+++ roles/php-fpm/default.rb
@@ -1,5 +1,7 @@
 include_recipe '../base'
 
+execute 'sudo apt-get update -y && apt-get dist-upgrade -y'
+
 include_cookbook 'git'
 include_cookbook 'nginx'
 include_cookbook 'php-fpm'
--- roles/rails/default.rb
+++ roles/rails/default.rb
@@ -1,5 +1,7 @@
 include_recipe '../base'
 
+execute 'sudo apt-get update -y && apt-get dist-upgrade -y'
+
 include_cookbook 'git'
 include_cookbook 'ruby'
 include_cookbook 'nginx'
